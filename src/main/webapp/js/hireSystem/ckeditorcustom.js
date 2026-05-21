83352c2385b107dc8072fad6e92347e4f73f3ddb
seok <hyungseok7581@gmail.com>
2026-05-21 18:03:45 +0900

    test

--- src/main/webapp/js/hireSystem/ckeditorcustom.js
+++ src/main/webapp/js/hireSystem/ckeditorcustom.js
@@ -19,6 +19,7 @@ class MyUploadAdapter {
 	}
 }
 
+
 function MyUploadAdapterPlugin(editor) {
 	editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
 		return new MyUploadAdapter(loader);
